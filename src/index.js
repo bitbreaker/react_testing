c5c3b347f1042bc93c103bc738cce0ad01028012
joepri <joepri@DANL0000-joepri.local>
2018-02-16 17:53:38 +0100

    first changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 const styles = {
@@ -6,30 +6,61 @@ const styles = {
   textAlign: 'center',
 };
 
-const App = () => (
-<div>
-    <table id="myTable">
-    <thead>
-    <tr>
-    <th>Name</th>
-    <th>Adresse</th>
-    </tr>
-    </thead>
-  <tr>
-    <td>Entry One</td>
-  </tr>
-  <tr>
-    <td>Entry Two</td>
-    </tr>
-    <tr>
-    <td>Entry Three</td>
-    </tr>
-    <tr>
-    <td>Entry Four</td>
-  </tr>
-  </table>
-  <button>SORT</button>
-</div>
-);
 
-render(<App />, document.getElementById('root'));
+
+var state = { items: ["Apfel", "Gurke", "Marmelade", "Brot", "Test"] };
+
+
+
+class Apps2 extends Component {
+
+  constructor(properties) {
+    super();
+    this.state = {
+      items: properties.datenElemente.items,
+    };
+  }
+
+  sortTable = () => {
+    const stateChanges = {
+      items: [].concat(this.state.items).sort()
+    };
+    this.setState(stateChanges);
+  }
+
+  render() {
+
+    return (
+      <div>
+        <table id="myTable">
+          <thead>
+            <tr>
+              <th>{this.state.items[0]}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.items.map(name =>
+              <tr key={name}>
+                <td>{name}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        <button onClick={this.sortTable}>Sort</button>
+      </div>
+    );
+  }
+}
+
+
+const Button = () => <button />;
+
+class Button2 extends Component {
+  render() {
+    return (
+      <button />
+    );
+  }
+}
+
+render(<Apps2 datenElemente={state} sort="ascending" />, document.getElementById('root'));
